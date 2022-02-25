75f2ea1c0285c08bc10f2ef1470f8f3b64726c81
csekok <csekok@gamma.ttk.pte.hu>
2022-02-25 11:24:08 +0100

    rename i and i1 to a and b in Calculator class

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,16 +1,29 @@
 package calculator;
 
 public class Calculator {
-    public static String add(int i, int i1) {
-        return "";
+    public static String add(int a, int b) {
+        int result = a + b;
+        return "" + a + " + " + b + " = " + result;
     }
-    public static String subtract(int i, int i1) {
-        return "";
+    public static String subtract(int a, int b) {
+        int result = a - b;
+        return "" + a + " - " + b + " = " + result;
     }
-    public static String multiply(int i, int i1) {
-        return "";
+    public static String multiply(int a, int b) {
+        int result = a * b;
+        return "" + a + " * " + b + " = " + result;
     }
-    public static String divide(int i, int i1) {
-        return "";
+    public static String divide(int a, int b) {
+        if (b == 0) {
+            return "Cannot divide with 0.";
+        } else {
+            if (a % b == 0) {
+                int result = a / b;
+                return "" + a + " / " + b + " = " + result;
+            } else {
+                double result = (double) a / b;
+                return "" + a + " / " + b + " = " + result;
+            }
+        }
     }
 }
