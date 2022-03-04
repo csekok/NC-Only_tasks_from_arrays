58548ad4d041127d257d72fec31949abca9d8af8
csekok <csekok@gamma.ttk.pte.hu>
2022-03-04 12:00:49 +0100

    task for 4th class

---
+++ src/main/java/numbers/NewNumbersMain.java
@@ -0,0 +1,114 @@
+package numbers;
+
+public class NewNumbersMain {
+    /**
+     * Task 0: Update the project:
+     *          - From menu: Git / "Update Project...", OR
+     *          - In right upper corner: blue arrow, OR
+     *          - Keyboard shortcut: Ctrl+T
+     * In "Update the project" popup click on Ok button.
+     */
+
+    /**
+     * Task 1: Create a main method.
+     * Create a generateArray method to generate a 1-dimensional array: 1 2 3 4 5
+     * Call this method and print the return value in main.
+     */
+
+    /**
+     * Task 2: Add a random parameter to generateArray method:
+     * - If the value of random parameter is true,
+     *   then return with a 5 element array filled with random numbers.
+     * - If the value of random parameter is false,
+     *   then return with the same array as before.
+     * Hint: You can generate random numbers with Random objects.
+     * Hint: Use nextInt(bound) method to get integers between 0 and bound value.
+     * Call the method with true and false parameters and print the results in main.
+     */
+
+    /**
+     * Task 3: Create a generateTwoDimensionalArray(random) method to generate a 2-dimensional array:
+     * - If the value of random parameter is true,
+     *   then return with a 2x3 array filled with random numbers.
+     * - If the value of random parameter is false, then return with array of this matrix:
+     *     1 2 3
+     *     4 5 6
+     * Call this method and print the return value in main.
+     */
+
+    /**
+     * Task 4: Create a generateList(random, length) method to generate
+     * a 1-dimensional array with dynamic length.
+     * - If the value of random parameter is true, then return with a list of random numbers.
+     *   The length of this list has to match with length parameter.
+     * - If the value of random parameter is false,
+     *   then return with an array with the numbers from 1 to value of length parameter.
+     * Hint: Use ArrayList.
+     */
+
+    /**
+     * Task 5: Create a print method to print all element of a list parameter to the console.
+     * For example: [1, 2, 3, 4, 5]
+     * Extra: Create similar method for 1- and 2-dimensional arrays.
+     */
+
+    /**
+     * Task 6: Create a selectEvenNumbers method, what return a subset of the list parameter.
+     * The subset list has to contains all and only the even numbers.
+     * Call this method in main and print a message with the length of subset list or
+     * the static "There are no even number in the list" message.
+     */
+
+    /**
+     * Task 7: Create an intersection method, what get 2 lists as parameters.
+     * The method has to return with a list, what contains all and only those elements,
+     * which is in both list parameters.
+     * Extra: Create same method for 1- and 2-dimensional arrays.
+     * Extra: Create same method for lists and arrays with double values.
+     */
+
+    /**
+     * Task 8: Create a separateEvenAndOdds method, what create 2 sublist of the list parameter.
+     * First sublist has to contains all and only the even numbers, and the second one
+     * has to contains all and only the odd numbers.
+     * Print to console:
+     *     - sublist of even numbers
+     *     - sublist of odd numbers
+     *     - length of sublist of even numbers
+     *     - length of sublist of odd numbers
+     *     - length of original list
+     * Hint: This method should have no return value, because you should print the values.
+     */
+
+    /**
+     * Task 9: Create a numberOfOdds(array) method to count how many odd value is in a list.
+     * Extra: Create same method for 1- and 2-dimensional arrays.
+     */
+
+    /**
+     * Task 10: Create a contains(array, value) method to decide the value is in a list or not.
+     * Extra: Create same method for 1- and 2-dimensional arrays.
+     * Extra: Create same method for lists and arrays with double values.
+     */
+
+    /**
+     * Task 11: Create a sum(array) method to get the sum of values in a list.
+     * Extra: Create same method for 1- and 2-dimensional arrays.
+     * Extra: Create same method for lists and arrays with double values.
+     */
+
+    /**
+     * Task 12: Create a indexOf(array, value) method to return the index of value in a list.
+     * If the list does not contain the value, then return with -1.
+     * Extra: Create same method for 1- and 2-dimensional arrays.
+     * Extra: Create same method for lists and arrays with double values.
+     */
+
+    /**
+     * Task 13: Create a union method, what get 2 lists as parameters.
+     * The method has to return with a list, what contains all elements of list parameters,
+     * but it has to contain all elements only 1 time (eliminate the duplications).
+     * Extra: Create same method for 1- and 2-dimensional arrays.
+     * Extra: Create same method for lists and arrays with double values.
+     */
+}
